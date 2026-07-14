d65bef446fb3722549c383e3c92807d0a477dce4
Henry <hgriffy@student.cscc.edu>
2026-07-14 08:10:51 -0400

    move user interaction to Controls class + misc comment

--- controls.py
+++ controls.py
@@ -14,18 +14,14 @@ class Controls:
         ###### READ DATA FROM CSV ######
 
     ###### PROMPT USER AND GATHER INPUT ######
-    def menu_and_response(self):
+    def do_menu_and_response(self):
         """
         Prompt user for input and return that input in variables
 
-        Returns:
-            do_what: which action the user wants to take
-            for_what: search string to use when searching
-            how_many: maximum number of lps to return (or 0 for all)
         """
         is_get_do_what_valid = False
         while not is_get_do_what_valid:
-            self.do_what: str = input("What would you like to do? q to quit, "
+            self.do_what = input("What would you like to do? q to quit, "
                                     "a to search by artist, t to search by title: ").lower()
             if len(self.do_what) == 1 and self.do_what in 'qat':
                 is_get_do_what_valid = True
@@ -35,10 +31,10 @@ class Controls:
                 self.for_what: str = input("Enter text to search for: ")
                 if self.for_what:
                     is_get_for_what_valid = True
-            self.how_many = input("How many results would you like? (zero to display all): ")
+            raw_how_many = input("How many results would you like? (zero to display all): ")
             # integer-checking code adapted from
             # https://www.geeksforgeeks.org/python/check-if-string-is-integer-in-python/
             try:
-                self.how_many = int(self.how_many)
+                self.how_many = int(raw_how_many)
             except ValueError:
-                self.how_many = len(lps)
+                self.how_many = 0
--- griffy_project02_lps.py
+++ griffy_project02_lps.py
@@ -27,64 +27,7 @@ import csv
 
 matching_indexes = []
 
-###### READ DATA FROM CSV ######
-def get_the_data(filename: str = 'lps.csv'):
-    """
-    Extract data from a csv file and return it for processing
 
-    Args:
-        filename: optional Name of csv file to get data from.
-
-    Returns:
-        get_lps: a list of dictionaries with all csv data
-        get_fields: a list of field names
-    """
-    get_fields = []  # Column names
-    get_lps = []    # Data rows
-    with open(filename, 'r') as csvfile:
-        csvreader = csv.DictReader(csvfile)  # Reader object
-
-        get_fields = next(csvreader)  # Read header
-        for row in csvreader:     # Read rows
-            get_lps.append(row)
-    return get_fields, get_lps
-
-    #print("Total no. of rows: %d" % csvreader.line_num)  # Row count
-
-    #for row in titles[:5]:
-    #    print(row)
-
-###### PROMPT USER AND GATHER INPUT ######
-def menu_and_response():
-    """
-    Prompt user for input and return that input in variables
-
-    Returns:
-        do_what: which action the user wants to take
-        for_what: search string to use when searching
-        how_many: maximum number of lps to return (or 0 for all)
-    """
-    get_do_what, get_for_what, get_how_many = "", "", 0
-    is_get_do_what_valid = False
-    while not is_get_do_what_valid:
-        get_do_what: str = input("What would you like to do? q to quit, "
-                                 "a to search by artist, t to search by title: ").lower()
-        if len(get_do_what) == 1 and get_do_what in 'qat':
-            is_get_do_what_valid = True
-    if get_do_what != 'q': #if user quitting, don't prompt for additional input
-        is_get_for_what_valid = False
-        while not is_get_for_what_valid:
-            get_for_what: str = input("Enter text to search for: ")
-            if get_for_what:
-                is_get_for_what_valid = True
-        get_how_many = input("How many results would you like? (zero to display all): ")
-        # integer-checking code adapted from
-        # https://www.geeksforgeeks.org/python/check-if-string-is-integer-in-python/
-        try:
-            get_how_many = int(get_how_many)
-        except ValueError:
-            get_how_many = len(lps)
-    return get_do_what, get_for_what, get_how_many
 
 ###### FIND MATCHING LPS ######
 def search_lps() -> list[int]:
